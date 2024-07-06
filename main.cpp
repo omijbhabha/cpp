489285d832053425fb4e231fe1f432bc6e1a961b
omijbhabha <omkarsingh5128@gmail.com>
2024-07-06 05:32:11 +0530

    Hi?!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    int lst[2][2]={{10,11},{12,13}};
+    int i,j=0;
+    for (i=0;i<2;i++){
+        for (j=0;j<2;j++){
+            cout<< *(*(lst+i)+j)<<" ";
+        }
+        cout<<endl;
+    }
 }   
